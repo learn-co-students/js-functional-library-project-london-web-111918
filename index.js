f7eaea7434c04c4ff29ad510b5212007b407a91e
Lingy94 <j.lingyx@gmail.com>
2019-01-08 17:25:45 +0000

    Done.

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ fi = (function() {
     },
 
     each: function(collection, callback) {
-        values = Object.values(collection)
-          for (let i=0; i < values.length; i++) {
-            /* for (const index in collection) {
-            //callback(collection[index])
-          }*/
-            callback(values[i])
-          }
-          return collection
-        },
+          values = Object.values(collection)
+            for (let i=0; i < values.length; i++) {
+              /* for (const index in collection) {
+              //callback(collection[index])
+            }*/
+              callback(values[i])
+            }
+            return collection
+          },
 
     map: function(collection, callback) {
-      const newCollection = [];
-        values = Object.values(collection)
-          for (let i=0; i < values.length; i++) {
-            /* for (const index in collection) {
-            //callback(collection[index])
-          }*/
-          let updated = callback(values[i])
-          newCollection.push(updated)
-          }
-          return newCollection
-        },
+          const newCollection = [];
+            values = Object.values(collection)
+              for (let i=0; i < values.length; i++) {
+                /* for (const index in collection) {
+                //callback(collection[index])
+              }*/
+              let updated = callback(values[i])
+              newCollection.push(updated)
+              }
+              return newCollection
+            },
 
 // function timesBy3(num) {num*3}
 
@@ -40,23 +40,23 @@ fi = (function() {
     },
 
     find: function(collection, predicate) {
-      values = Object.values(collection)
-        for (let i=0; i < values.length; i++) {
-          if (predicate(values[i] /*== true */))
-          return values[i]
-        }
-        // return undefined
-    },
+          values = Object.values(collection)
+            for (let i=0; i < values.length; i++) {
+              if (predicate(values[i] /*== true */))
+              return values[i]
+            }
+            // return undefined
+        },
 
     filter: function(collection, predicate) {
-      const newCollection = []
-      values = Object.values(collection)
-        for (let i=0; i < values.length; i++) {
-          if (predicate(values[i] /*== true */))
-          newCollection.push(values[i])
-        }
-        return newCollection
-    },
+            const newCollection = []
+            values = Object.values(collection)
+              for (let i=0; i < values.length; i++) {
+                if (predicate(values[i] /*== true */))
+                newCollection.push(values[i])
+              }
+              return newCollection
+          },
 
     size: function(collection){
       keys = Object.keys(collection)
@@ -64,20 +64,20 @@ fi = (function() {
     },
 
     first: function(collection, n=1) {
-      const newCollection = [];
-        values = Object.values(collection)
-          for (let i=0; i < n; i++) {
-            /* for (const index in collection) {
-            //callback(collection[index])
-          }*/
-          // let updated = values[i]
-          newCollection.push(values[i])
-            if (n == 1){
-              return newCollection[0]
-            }
-          }
-          return newCollection
-        },
+            const newCollection = [];
+              values = Object.values(collection)
+                for (let i=0; i < n; i++) {
+                  /* for (const index in collection) {
+                  //callback(collection[index])
+                }*/
+                // let updated = values[i]
+                newCollection.push(values[i])
+                  if (n == 1){
+                    return newCollection[0]
+                  }
+                }
+                return newCollection
+              },
 
       last: function(collection, n=1) {
         const newCollection = [];
@@ -128,45 +128,45 @@ fi = (function() {
        },
 
        uniq: function(array, isSorted, callback) {
-         const callbackArray = []
-         const newArray = []
-         if (!isSorted) {
-           if (callback) {
-             for (const index in array) {
-               let newVariable = callback(array[index])
-               if (!callbackArray.includes(newVariable)) {
-                 callbackArray.push(newVariable)
-                 newArray.push(array[index])
+             const callbackArray = []
+             const newArray = []
+             if (!isSorted) {
+               if (callback) {
+                 for (const index in array) {
+                   let newVariable = callback(array[index])
+                   if (!callbackArray.includes(newVariable)) {
+                     callbackArray.push(newVariable)
+                     newArray.push(array[index])
+                     }
+                   }
+                 } else {
+                   for (const index in array) {
+                     if (!newArray.includes(array[index])) {
+                       newArray.push(array[index])
+                       }
+                   }
                  }
                }
-             } else {
-               for (const index in array) {
-                 if (!newArray.includes(array[index])) {
-                   newArray.push(array[index])
-                   }
-               }
-             }
-           }
-           return newArray
-       },
+               return newArray
+           },
 
         keys: function(object) {
-          return Object.keys(object)
+              return Object.keys(object)
         },
         //
         values: function(object) {
-          return Object.values(object)
+                return Object.values(object)
         },
         //
         functions: function(object) {
-        const functions = []
-          for (const index in object) {
-            if (typeof object[index] === 'function') {
-              functions.push(index)
-              }
-          }
-          return functions.sort()
-        },
+                    const functions = []
+                      for (const index in object) {
+                        if (typeof object[index] === 'function') {
+                          functions.push(index)
+                          }
+                      }
+                      return functions.sort()
+                    },
 }
 
 })()
